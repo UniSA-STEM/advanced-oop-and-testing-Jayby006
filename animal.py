79eb34a4ba1c6e79092ebd2c466db535eff6c147
jayangabandara <jayby006@mymail.unisa.edu.au>
2025-11-22 22:23:02 +1030

    Creates Animal class with basic attributes, getters and setters

--- animal.py
+++ animal.py
@@ -1,8 +1,43 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: animal.py
+Description: Includes animal classes and their basic actions and health infomation
+Author: Jayanga Madushanka Bandara Bathabure Gedara
+ID: 110432974
+Username: jayby006
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Animal:
+     def __init__(self, name, species, age, diet, category):
+        self.__name = name
+        self.__species = species
+        self.__age = age
+        self.__diet = diet
+        self.__category = category          
+        self.__enclosure = None           
+        self.__health_issues = []       
+
+
+     def get_name(self):
+        return self.__name
+     
+     def get_species(self):
+        return self.__species
+
+     def get_age(self):
+        return self.__age
+
+     def get_diet(self):
+        return self.__diet
+
+     def get_category(self):
+        return self.__category
+
+     def get_enclosure(self):
+        return self.__enclosure
+
+     def get_health_issues(self):
+        return self.__health_issues
+     
+     def set_enclosure(self, enclosure):
+        self.__enclosure = enclosure
