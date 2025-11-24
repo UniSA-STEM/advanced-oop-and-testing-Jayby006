9c01cb9071a043f07f6cbb7c45d7d619ff4da84d
jayangabandara <jayby006@mymail.unisa.edu.au>
2025-11-24 19:49:33 +1030

    Add comments to staff.py

--- staff.py
+++ staff.py
@@ -12,12 +12,16 @@ from enclosure import Enclosure
 
 
 class Staff:
+
+    """
+    The Staff class stores basic information about a staff member and which animals and enclosures they are responsible for.
+    """
     def __init__(self, name, staff_id):
         self.__name = name
         self.__staff_id = staff_id
         self.__assigned_enclosures = []
         self.__assigned_animals = []
-
+    
     def get_name(self):
         return self.__name
 
@@ -30,7 +34,7 @@ class Staff:
     def get_assigned_animals(self):
         return self.__assigned_animals
 
-    def assign_enclosure(self, enclosure):
+    def assign_enclosure(self, enclosure): #Assign an enclosure to this staff member and Does not add duplicates.
         if enclosure not in self.__assigned_enclosures:
             self.__assigned_enclosures.append(enclosure)
 
@@ -40,17 +44,17 @@ class Staff:
 
 class Zookeeper(Staff):
 
-    def feed_animal(self, animal):
+    def feed_animal(self, animal):   # zookeeper feeds the given animal
         print(self.get_name() + " feeds " + animal.get_name())
         animal.eat()
 
-    def clean_enclosure(self, enclosure):
+    def clean_enclosure(self, enclosure):    # zookeeper cleans the given enclosure
         print(self.get_name() + " cleans " + enclosure.get_name())
         enclosure.clean()
 
 class Veterinarian(Staff):
 
-    def health_check(self, animal):
+    def health_check(self, animal):  #Print all recorded health issues for the animal and if there are none, show a simple message.
         print(self.get_name() + " checks " + animal.get_name())
         issues = animal.get_health_issues()
 
@@ -63,7 +67,7 @@ class Veterinarian(Staff):
                     "| Severity:", issue["severity"],
                     "| Active:", issue["active"])
                 
-    def create_treatment(self, animal, description, date_reported, severity, treatment):
+    def create_treatment(self, animal, description, date_reported, severity, treatment):  #Record a new health issue for the animal using the animal's add_health_issue method.
         message = self.get_name() + " records health issue for " + animal.get_name()
         print(message)
-        animal.add_health_issue(description, date_reported, severity, treatment)
+        animal.add_health_issue(description, date_reported, severity, treatment)       
