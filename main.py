177ca483eecbe5d46f8352a42d041c654181b81e
jayangabandara <jayby006@mymail.unisa.edu.au>
2025-11-24 15:45:18 +1030

    Add clear spacing and header for output.

--- main.py
+++ main.py
@@ -14,26 +14,29 @@ from staff import Zookeeper, Veterinarian
 
 def main():
 
-    print("Creating animals...")
+    print("=== Creating animals... ===")
     lion = Mammal("Leo", "Lion", 5, "Meat")
     parrot = Bird("Polly", "Parrot", 2, "Seeds")
     snake = Reptile("Snake", "Python", 3, "Rodents")
     print("Created animals:", lion.get_name(), ",", parrot.get_name(), ",", snake.get_name())
+    print()
 
-    print("Creating enclosures...")
+    print("=== Creating enclosures... ===")
     savannah = Enclosure("Savannah Plains", "Savannah", 500, "mammal")
     aviary = Enclosure("Tropical Aviary", "Rainforest", 200, "bird")
     reptile_house = Enclosure("Reptile House", "Desert", 150, "reptile")
     print("Created enclosures:", savannah.get_name(), ",", aviary.get_name(), ",", reptile_house.get_name())
+    print()
 
-    print("Creating staff...")
+    print("=== Creating staff...===")
     keeper = Zookeeper("Jayanga", "ZK001")
     vet = Veterinarian("Mark", "VT001")
     print("Created staff:", keeper.get_name(), "(Zookeeper),", vet.get_name(), "(Veterinarian)")
+    print()
 
 
 
-    print("Assigning staff to enclosures and animals...")
+    print("=== Assigning staff to enclosures and animals...===")
     keeper.assign_enclosure(savannah)
     keeper.assign_enclosure(aviary)
     keeper.assign_enclosure(reptile_house)
@@ -42,23 +45,28 @@ def main():
     vet.assign_animal(snake)
     print(keeper.get_name() + " assigned to all enclosures.")
     print(vet.get_name() + " assigned to", lion.get_name(), "and", snake.get_name())
+    print()
 
-    print("Adding animals to enclosures...")
+    print("=== Adding animals to enclosures... === ")
     savannah.add_animal(lion)
     aviary.add_animal(parrot)
     reptile_house.add_animal(snake)
+    print()
 
-    print("Zookeeper feeds animals and cleans enclosures...")
+
+    print("=== Zookeeper feeds animals and cleans enclosures... ===")
     keeper.feed_animal(lion)
     keeper.feed_animal(parrot)
 
     keeper.clean_enclosure(savannah)
     keeper.clean_enclosure(aviary)
     keeper.clean_enclosure(reptile_house)
+    print()
 
-    print("Veterinarian health checks...")
+    print("=== Veterinarian health checks...===")
     print("Initial health check for Leo:")
     vet.health_check(lion)
+    print()
 
     print("Recording a health issue for Leo...")
     vet.create_treatment(
@@ -68,17 +76,22 @@ def main():
         "moderate",
         "Rest and medicine for 5 days."
     )
+    print()
 
     print("Health check for Leo after recording issue:")
     vet.health_check(lion)
+    print()
 
-    print("Trying to move Leo (under treatment) to the aviary...")
+    print("=== Trying to move Leo (under treatment) to the aviary...===")
     aviary.add_animal(lion)  
+    print()
 
     print("Final enclosure status reports:")
     print(savannah.get_status())
     print(aviary.get_status())
     print(reptile_house.get_status())
+    print()
+
 
 if __name__ == "__main__":
     main()
