a9e9d0c38f43eb5f8247e6fa08c772ebf58f0244
jayangabandara <jayby006@mymail.unisa.edu.au>
2025-11-24 19:05:16 +1030

    Unit test successful, adding correct animal reject wrong animal category in enclosure

--- test_zoo.py
+++ test_zoo.py
@@ -17,5 +17,18 @@ def test_animal_health_issue():
     assert parrot.has_active_issue() is True
 
 
+def test_enclosure_add_correct_category():
+    lion = Mammal("Leo", "Lion", 5, "Meat")
+    savannah = Enclosure("Savannah Plains", "Savannah", 500, "mammal")
 
+    savannah.add_animal(lion)
+    assert lion in savannah.get_animals()
+
+
+def test_enclosure_reject_wrong_category():
+    parrot = Bird("Polly", "Parrot", 2, "Seeds")
+    savannah = Enclosure("Savannah Plains", "Savannah", 500, "mammal")
+
+    savannah.add_animal(parrot)   
+    assert parrot not in savannah.get_animals()
 
